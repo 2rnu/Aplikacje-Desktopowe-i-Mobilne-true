d6934257bc1df06a1c231c6ea4b82b6b5b6116f0
Uczeń <Ucze@DESKTOP-RGP55N4>
2024-10-23 15:52:24 +0200

    jd

---
+++ Aplikacje-2023-2024-2025/Aplikacje-desktopowe-i-mobilne/ConverterMauiApp/Converters/BoolToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterMauiApp.Converters
+{
+    internal class BoolToColorConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+#pragma warning disable CS8605 // Unboxing a possibly null value.
+            bool isChecked = (bool)value;
+#pragma warning restore CS8605 // Unboxing a possibly null value.
+            if (isChecked)
+                return new Color(1f, 0f, 0f); //Color.Red - inaczej
+            else return new Color(0f, 1f, 0f); //Color.Green - inaczej
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Aplikacje-2023-2024-2025/Aplikacje-desktopowe-i-mobilne/ConverterMauiApp/Converters/BoolToTextConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterMauiApp.Converters
+{
+    class BoolToTextConverter : IValueConverter
+    {
+
+        public string? TrueMewssage { get; set; }
+        public string? FalseMessage { get; set; }
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+#pragma warning disable CS8605 // Unboxing a possibly null value.
+            bool isChecked = (bool)value;
+#pragma warning restore CS8605 // Unboxing a possibly null value.
+            
+            if (isChecked)
+                return TrueMessage;
+            else return FalseMessage;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
