8cf13313fa2d95c2cabec38b19bb061824263d99
Sala213 <Sala213@ezse.pl>
2025-09-11 09:39:16 +0200

    Wszystko i nic

---
+++ Aplikacje-2023-2024-2025/Aplikacje-desktopowe-i-mobilne/TabPageMauiApp/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace TabPageMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            TabbedPage tabbedPage = new TabbedPage();
+            tabbedPage.Children.Add(new MainPage() {Title = "Główna" });
+            tabbedPage.Children.Add(new AnotherPage());
+            tabbedPage.Children.Add(new AboutPage() { IconImageSource = "pc_ver_1.png" });
+
+
+
+            return new Window(tabbedPage);
+        }
+    }
+}
---
+++ Aplikacje-2023-2024-2025/Aplikacje-desktopowe-i-mobilne/TabPageMauiApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TabPageMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
